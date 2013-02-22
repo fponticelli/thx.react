ebba9cefae80f376bc926cc28a3e7bfcf45118e0
Franco Ponticelli <franco.ponticelli@gmail.com>
2013-02-21 18:24:15 -0700

    added Binder 0 to 5 classes

--- src/thx/react/Binder.hx
+++ src/thx/react/Binder.hx
@@ -82,3 +82,108 @@ class Binder extends BaseBinder
 	public function off<T1>(names : String, ?handler : T1 -> Void)
 		unbind(names, handler);
 }
+
+class Binder0 extends BaseBinder
+{
+	public function trigger(names : String)
+		dispatch(names, []);
+
+	public function on(names : String, handler : Void -> Void)
+		bind(names, handler);
+
+	public function one(names : String, handler : Void -> Void)
+	{
+		function h() {
+			unbind(names, h);
+			handler();
+		}
+		bind(names, h);
+	}
+
+	public function off(names : String, ?handler : Void -> Void)
+		unbind(names, handler);
+}
+
+class Binder2 extends BaseBinder
+{
+	public function trigger<T1, T2>(names : String, payload1 : T1, payload2 : T2)
+		dispatch(names, [payload1, payload2]);
+
+	public function on<T1, T2>(names : String, handler : T1 -> T2 -> Void)
+		bind(names, handler);
+
+	public function one<T1, T2>(names : String, handler : T1 -> T2 -> Void)
+	{
+		function h(v1 : T1, v2 : T2) {
+			unbind(names, h);
+			handler(v1, v2);
+		}
+		bind(names, h);
+	}
+
+	public function off<T1, T2>(names : String, ?handler : T1 -> T2 -> Void)
+		unbind(names, handler);
+}
+
+class Binder3 extends BaseBinder
+{
+	public function trigger<T1, T2, T3>(names : String, payload1 : T1, payload2 : T2, payload3 : T3)
+		dispatch(names, [payload1, payload2, payload3]);
+
+	public function on<T1, T2, T3>(names : String, handler : T1 -> T2 -> T3 -> Void)
+		bind(names, handler);
+
+	public function one<T1, T2, T3>(names : String, handler : T1 -> T2 -> T3 -> Void)
+	{
+		function h(v1 : T1, v2 : T2, v3 : T3) {
+			unbind(names, h);
+			handler(v1, v2, v3);
+		}
+		bind(names, h);
+	}
+
+	public function off<T1, T2, T3>(names : String, ?handler : T1 -> T2 -> T3 -> Void)
+		unbind(names, handler);
+}
+
+class Binder4 extends BaseBinder
+{
+	public function trigger<T1, T2, T3, T4>(names : String, payload1 : T1, payload2 : T2, payload3 : T3, payload4 : T4)
+		dispatch(names, [payload1, payload2, payload3, payload4]);
+
+	public function on<T1, T2, T3, T4>(names : String, handler : T1 -> T2 -> T3 -> T4 -> Void)
+		bind(names, handler);
+
+	public function one<T1, T2, T3, T4>(names : String, handler : T1 -> T2 -> T3 -> T4 -> Void)
+	{
+		function h(v1 : T1, v2 : T2, v3 : T3, v4 : T4) {
+			unbind(names, h);
+			handler(v1, v2, v3, v4);
+		}
+		bind(names, h);
+	}
+
+	public function off<T1, T2, T3, T4>(names : String, ?handler : T1 -> T2 -> T3 -> T4 -> Void)
+		unbind(names, handler);
+}
+
+class Binder5 extends BaseBinder
+{
+	public function trigger<T1, T2, T3, T4, T5>(names : String, payload1 : T1, payload2 : T2, payload3 : T3, payload4 : T4, payload5 : T5)
+		dispatch(names, [payload1, payload2, payload3, payload4, payload5]);
+
+	public function on<T1, T2, T3, T4, T5>(names : String, handler : T1 -> T2 -> T3 -> T4 -> T5 -> Void)
+		bind(names, handler);
+
+	public function one<T1, T2, T3, T4, T5>(names : String, handler : T1 -> T2 -> T3 -> T4 -> T5 -> Void)
+	{
+		function h(v1 : T1, v2 : T2, v3 : T3, v4 : T4, v5 : T5) {
+			unbind(names, h);
+			handler(v1, v2, v3, v4, v5);
+		}
+		bind(names, h);
+	}
+
+	public function off<T1, T2, T3, T4, T5>(names : String, ?handler : T1 -> T2 -> T3 -> T4 -> T5 -> Void)
+		unbind(names, handler);
+}
